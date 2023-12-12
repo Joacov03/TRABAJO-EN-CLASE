e59dc186169a000986a5116cc47d2f2f85c15108
Joacov03 <153627968+Joacov03@users.noreply.github.com>
2023-12-12 11:15:33 -0500

    Add files via upload

---
+++ PAE2.py
@@ -0,0 +1,22 @@
+#El propósito de este programa es practicar y aplicar todo lo aprendido en clases 
+#ejercicio1
+sint= 5+8 #suma enteros
+rflt=7.38-2.4 #resta flotantes
+mint= 3*2 #multiplicación enteros
+dflt=4.9/3.2 #división flotantes
+print(sint, rflt, mint, dflt)#Se imprimen todas las variables
+#sint es la suma de dos números enteros, rflt es la resta de dos flotantes, mint la multiplicación de enteros y dflt división de flotantes
+#ejercicio 2
+nombre= "Joaquín Vélez"
+a= len(nombre) #Nos muestra la longitud y lo guardamos en una variable
+print(nombre, a) #Se imprime el nombre y su longitud
+#Este programa es para mostrar mi nombre completo y su longitud
+#Ejercicio 3
+vent=6#variable entera
+vflo=3.8#variable flotante
+intflt= float(vent)# se cambia de entero a flotante
+fltint= int(vflo)# se cambia de flotante a entero
+print(vent, vflo, intflt, fltint)# se imprimen las variables iniciales y después de la conversión
+
+
+
